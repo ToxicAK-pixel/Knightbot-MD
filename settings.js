41ee3542d1fbc51fccaeb8bb20d46ecde54c5446
ToxicAK-pixel <toxicalex90@gmail.com>
2025-04-22 15:54:45 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ const settings = {
   botOwner: 'Serious Joker', // Your name
   ownerNumber: '+254785787612', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "private",
+  commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
